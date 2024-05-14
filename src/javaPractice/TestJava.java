849d46e8201119e29f55cd5df69fb83093e28e50
Dinesh <dineshdk.tdkm@gmail.com>
2024-05-14 21:34:23 +0400

    First commit

---
+++ src/javaPractice/TestJava.java
@@ -0,0 +1,24 @@
+package javaPractice;
+
+public class TestJava {
+	//systemOutPrintLnDetails
+	
+	static String s="java";
+	public static void mail(String[] args)
+	{
+		//TestJava.s.length();
+	// here Test Java is class name
+		//s is static varibale which is present in TestJava class
+		//length() is method which is present in String class
+		System.out.println();
+		
+		//System is class name which is present in java.long.package
+		// out is static variable which is present in System class of type PrintStream
+		// println is method which is present print stream class.
+		
+		char ch='a';
+		System.out.println(ch);
+		
+	}
+	
+}
