66c7372fcca280ee124578c4418f3a27b07e9321
Dinesh <dineshdk.tdkm@gmail.com>
2024-05-14 21:46:48 +0400

    Adding Hello world1 class commit

---
+++ src/javaPractice/HelloWorld1.java
@@ -0,0 +1,10 @@
+package javaPractice;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world");
+	}
+
+}
